08bb70676ce1102b28c41df9ebc119a06862f43f
DESKTOP-LCOVIGJ\user <dlqudwp21@naver.com>
2023-10-19 18:04:08 +0900

    [이병제] - AllInMall 프로젝트 깃연동을 위한 최초의 커밋

---
+++ src/main/java/com/dlqudwp/allin/AllInMallApplication.java
@@ -0,0 +1,13 @@
+package com.dlqudwp.allin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllInMallApplication.class, args);
+	}
+
+}
